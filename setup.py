27d13a60802797e74807a62523f3378124889e86
Jack Kolb <jkolb@berkeley.edu>
2015-11-01 16:37:41 -0800

    Improved setup.py and directory structure

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="bw2python",
     version="0.1",
-    description="Python bindings for Bosswave",
+    description="Python bindings for Bosswave 2",
     author="Jack Kolb",
-    url="https://github.com/jKolb1/bw2python",
-    packages=["bw2python"]
+    url="https://github.com/SoftwareDefinedBuildings/bw2python",
+    packages=["bw2python"],
+    package_dir={'bw2python': 'src'},
 )

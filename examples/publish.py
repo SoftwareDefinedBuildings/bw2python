9dd7e2a0cfe29f58601bce24cd020fb3274db63e
Jack Kolb <jkolb@berkeley.edu>
2016-06-03 20:00:59 -0700

    Update examples to new API

--- examples/publish.py
+++ examples/publish.py
@@ -2,10 +2,10 @@ from bw2python import potypes
 from bw2python.bwtypes import PayloadObject
 from bw2python.client import Client
 
-bw_client = Client('localhost', 28589)
-bw_client.connect()
+bw_client = Client()
 bw_client.setEntityFromEnviron()
 
-po = PayloadObject(potypes.PO_DF_TEXT, None, "Hello, World!")
 for i in range(10):
+    msg = "{}: Hello, World!".format(i)
+    po = PayloadObject(potypes.PO_DF_TEXT, None, msg)
     bw_client.publish("scratch.ns/demo", auto_chain=True, payload_objects=(po,))
--- examples/subscribe.py
+++ examples/subscribe.py
@@ -4,14 +4,14 @@ from bw2python import potypes
 from bw2python.bwtypes import PayloadObject
 from bw2python.client import Client
 
-bw_client = Client('localhost', 28589)
-bw_client.connect()
+bw_client = Client()
 bw_client.setEntityFromEnviron()
 
 def onMessage(bw_message):
     for po in bw_message.payload_objects:
         if po.type_dotted == potypes.PO_DF_TEXT:
             print po.content
+
 bw_client.subscribe("scratch.ns/demo", onMessage)
 
 print "Subscribing. Ctrl-C to quit."
